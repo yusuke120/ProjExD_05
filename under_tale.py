71d758fd1e7ef26853cd9a93098d315cba124bd6
ogasho24 <onome24@icloud.com>
2023-10-31 23:28:31 +0900

    コメント追加や整理済み

--- under_tale.py
+++ under_tale.py
@@ -8,25 +8,28 @@ import pygame as pg
 WIDTH = 800
 HEIGHT = 500
 
-
+# HP関連の機能を実装するためのクラス
 class Hp:
     def __init__(self):
         self.width = 100
 
-    def genfont(self, txt):
+    #図形を生成するためのインスタンス
+    def genfont(self, txt): 
         self.fonto = pg.font.Font(None, 25)
         self.txt = self.fonto.render(txt, True, (255, 255, 255))
-    # pg.draw.rect(hpbar_sur, (255, 0, 0), (50, 0, 100, 20))
+
+    #　文字を生成するためのインスタンス
     def genobj(self, color, locate, hpbar_sur: pg.Surface):
         self.color = color
         self.locate = locate
         self.rct = pg.draw.rect(hpbar_sur, color, locate)
 
+    # 文字と図形で違う処理で更新するインスタンス
     def update(self, hpbar_sur: pg.Surface, xy=None, txt=None):
-        if txt:
+        if txt: # 文字を生成
             self.obj = self.fonto.render(txt, True, (255, 255, 255))
             hpbar_sur.blit(self.obj, xy)
-        elif self.rct:
+        elif self.rct: #　図形を生成
             print(self.locate)
             pg.draw.rect(hpbar_sur, self.color, self.locate)
         
@@ -39,12 +42,10 @@ def main():
     clock = pg.time.Clock()
     pg.draw.rect(sikaku1, (255, 255, 255), (0, 0, 400, 200))
     pg.draw.rect(sikaku1, (0, 0, 0), (5, 5, 390, 190))
+
+    #　ここからhpクラスオブジェクトを生成
     hpbar_width = 100
     hpbar_sur = pg.Surface((250, 50)) # HPバーが表示される空間
-    # hp_fonto = pg.font.Font(None, 25)
-    # hp_txt_num = hp_fonto.render(f"{hpbar_width}/100", True, (255, 255, 255))
-    # hp_txt = hp_fonto.render("HP", True, (255, 255, 255))
-    # hpbar_sur.blit(hp_txt, [20, 5])
     hp_HP = Hp()
     hp_HP.genfont("HP")
     hp_num = Hp()
@@ -55,7 +56,7 @@ def main():
     hp_bar_green.genobj((0, 255, 0), (50, 0, hpbar_width, 20), hpbar_sur)
     hp_bar_black = Hp()
     hp_bar_black.genobj((1, 1, 1), (170, 0, 100, 20), hpbar_sur)
-    hps = pg.sprite.Group()
+    #　ここまで
 
 
 
@@ -65,25 +66,22 @@ def main():
     while True:
         hp_bar_green.width = hp_bar_green.width - tmr/100
         hp_bar_green.locate = (50, 0, hp_bar_green.width, 20)
-        # hp_txt_num = hp_fonto.render(f"{hpbar_width}/100", True, (255, 255, 255))
         screen.blit(sikaku1, (200, 200))
         screen.blit(hpbar_sur, (250, 405)) # hpbar_surの表示位置の指定
-        # hpbar_sur.blit(hp_txt_num, [170, 5])
+        hpbar_sur.set_colorkey((0, 0, 0)) 
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return 0
-        
-        # pg.draw.rect(hpbar_sur, (255, 0, 0), (50, 0, 100, 20)) # HPバー（赤）を表示
-        # pg.draw.rect(hpbar_sur, (0, 255, 0), (50, 0, hpbar_width, 20))  # HPバー（緑）を表示
-        hpbar_sur.set_colorkey((0, 0, 0))  
-
+         
+        #ここからhpクラスオブジェクトを更新
         hp_bar_black.update(hpbar_sur)
         hp_bar_red.update(hpbar_sur)
         hp_bar_green.update(hpbar_sur)
         hp_HP.update(hpbar_sur, [20, 5], "HP")
         hp_num.update(hpbar_sur, [170, 5], f"{hp_bar_green.width}/100")
-        
+        #　ここまで
+
         pg.display.update()
         tmr += 1
         clock.tick(30)
